0b29a68616357b080ae77bdfe6fcb622a6500f03
SabbirSami <smd71430@gmail.com>
2022-02-17 21:18:29 +0600

    final

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@ function getInputValue(inputId){
     let incomeValue = parseFloat(incomeText.value);
     return incomeValue
 }
+function getError(errorId){
+    let error = document.getElementById(errorId).classList.remove('d-none');
+    return error;
+}
+function removeError(removeErrorId){
+    let removeError = document.getElementById(removeErrorId).classList.add('d-none');
+    return removeError;
+}
 
 // TOTAL EXPENSES MULTIPLICATION-------------------
 function getTotalCost(){
@@ -12,23 +20,32 @@ function getTotalCost(){
 
     if(foodCost >= 0 && rentCost >= 0 && clothCost >= 0){
         let totalCost = foodCost + rentCost + clothCost;
-        document.getElementById('invalid-worning').classList.add('d-none');
+        removeError('invalid-worning');
         return totalCost;
     }
     else{
-        document.getElementById('invalid-worning').classList.remove('d-none');
+        getError('invalid-worning');
+        return 0;
     }
 }
 function getBalance (){
     let incomeValue = getInputValue('income-input');
     if(incomeValue > 0){
         let totalBalance = incomeValue - getTotalCost();
-        return totalBalance;
+        if(totalBalance < 0){
+            getError('balance-error');
+            return 0;
+        }
+        else{
+            return totalBalance;
+        }
     }
     else{
-        document.getElementById('invalid-worning').classList.remove('d-none');
+        getError('invalid-worning');
     }
 }
+
+
 
 document.getElementById('calculate-button').addEventListener('click', function(){  
     let totalCost = getTotalCost()
@@ -42,11 +59,11 @@ document.getElementById('calculate-button').addEventListener('click', function()
     //BALANCE
     if(totalCost >= 0){
         let balanceFild = document.getElementById('balance');
-        document.getElementById('balance-error').classList.add('d-none')
+        removeError('balance-error');
         balanceFild.innerText = getBalance();
     }
     else{
-        document.getElementById('balance-error').classList.remove('d-none')
+        getError('balance-error');
     }
 })
 
@@ -54,24 +71,29 @@ document.getElementById('calculate-button').addEventListener('click', function()
 document.getElementById('save-button').addEventListener('click',function(){
     let saveIncomeValue = getInputValue('save_income');
     let incomeValue = getInputValue('income-input');
-
-    let savingAmount = (incomeValue * saveIncomeValue) / 100;
-    let balance = getBalance();
-    if(saveIncomeValue >0){
-        if(savingAmount > balance){
-            document.getElementById('save-worning').classList.remove('d-none');
-        }
-        else{
-            // SAVING AMOUNT
-            let totalSaved = document.getElementById('saved');
-            document.getElementById('save-worning').classList.add('d-none');
-            totalSaved.innerText = savingAmount;
-            //REMAINING BALANCE--------------------------------
-            let remainingBalance = document.getElementById('remaining-balance');
-            remainingBalance.innerText = getBalance() - totalSaved.innerText
-        }    
+    if(saveIncomeValue == '' || saveIncomeValue <= 0 ){
+        getError('save-worning');
     }
     else{
-        document.getElementById('save-error').classList.remove('d-none');
+        let savingAmount = (incomeValue * saveIncomeValue) / 100;
+        let balance = getBalance();
+        if(saveIncomeValue >0){
+            if(savingAmount > balance){
+                getError('save-worning');
+            }
+            else{
+                // SAVING AMOUNT
+                let totalSaved = document.getElementById('saved');
+                removeError('save-worning');
+                removeError('save-error');
+                totalSaved.innerText = savingAmount;
+                //REMAINING BALANCE--------------------------------
+                let remainingBalance = document.getElementById('remaining-balance');
+                remainingBalance.innerText = balance - totalSaved.innerText
+            }    
+        }
+        else{
+            getError('save-error');
+        }
     }
 })
